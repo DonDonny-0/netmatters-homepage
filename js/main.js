aee2534f8f6bad5350ef2569373a3c1877047370
Brandon Thomas Murray <brand.t2004@icloud.com>
2025-11-19 15:51:51 +0000

    sidebar fully implemented, stickty header too, cookies next then testing

--- js/main.js
+++ js/main.js
@@ -1,17 +1,41 @@
-
 $(document).ready(function() {
 
   // jquery for expand and collapse the sidebar
   $('#hamburger').click(function() {
-    $('#container').stop();
-    $('#container').animate({left: '-350px'});
+    $('body').addClass('overflow-hidden');
+    $('#open-sidebar').css('visibility', 'visible')
+    if (window.innerWidth < 1025) {
+      $('#container').animate({left: '-275px'});
+      if ($('.sidebar').innerWidth === '350px') {
+        $('#container').animate({left: '-350px'});
+      }
+    }
+    else {
+      $('#container').animate({left: '-350px'});
+      if ($('.sidebar').innerWidth === '275px') {
+        $('#container').animate({left: '-275px'});
+      }
+    }
     $('#container').addClass('active');
   });
 
-  // $('#container').click(function () {
-  //   $('#container').animate({left: '0'});
-  //   $("#container").removeClass('active');
-  // });
+  while ($('#container').hasClass('active')) {
+    if ($('.sidebar').innerWidth === '350px') {
+      $('#container').animate({left: '-350px'});
+    }
+    else {
+      $('#container').animate({left: '-275px'});
+    }
+  }
+
+
+  $('#open-sidebar').click(function () {
+    $('#open-sidebar').css('visibility', 'hidden')
+    $('#container').animate({left: '0'});
+    $("#container").removeClass('active');
+    $('body').removeClass('overflow-hidden');
+  });
+
 
   $('.banner-carousel').slick({
     dots: true,
@@ -19,12 +43,19 @@ $(document).ready(function() {
     autoplay: true,
     autoplaySpeed: 4000
   })
-});
 
-const body = document.querySelector('body');
-body.addEventListener('scroll', () => {
-  $('header').animsition({
-    inClass: 'fade-in-down-lg',
-    inDuration: 500
-  })
-})
+  const stickyHeader = document.querySelector('.sticky-header');
+
+  var lastScrollTop = 0;
+
+  window.addEventListener('scroll', () => {
+    var st = window.pageYOffset || document.documentElement.scrollTop;
+    if (st < lastScrollTop && window.pageYOffset > 400) {
+      $('.sticky-header').sticky();
+    }
+    else if (st > lastScrollTop) {
+      $('.sticky-header').unstick();
+    }
+    lastScrollTop = st <= 0 ? 0 : st;
+  });
+});
